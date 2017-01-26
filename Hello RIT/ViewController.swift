2832119d5261112e412bff28fb1a6aa4d5f59091
Ben Wilcox <reccanti@gmail.com>
2017-01-26 12:01:31 -0500

    Initial Commit
    Initial commit for this repo

--- Hello RIT/ViewController.swift
+++ Hello RIT/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // these are ivars - instance variables
+    var topLabel: UILabel = UILabel()
+    var bottomLabel:UILabel = UILabel()
+    var authorLabel:UILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // top label code
+        topLabel.text = "Hello"
+        topLabel.font = UIFont.systemFont(ofSize: 36)
+        topLabel.sizeToFit()
+        
+        topLabel.center = CGPoint(x: 100, y: 40)
+        view.addSubview(topLabel)
+        
+        // bottom label code
+        bottomLabel.text = "RIT"
+        bottomLabel.font = UIFont.italicSystemFont(ofSize: 48)
+        bottomLabel.sizeToFit()
+        bottomLabel.center = CGPoint(x: 200, y:90)
+        
+        view.addSubview(bottomLabel)
+        
+        // color orange
+        view.backgroundColor = UIColor.orange
+        // view.backgroundColor = UIColor(colorLiteralRed: 0.2, green: 0.5, blue: 0.8, alpha: 1.0)
+        
+        // add name to the center of the screen programmatically
+        authorLabel.text = "Ben Wilcox"
+        authorLabel.textColor = UIColor.darkGray
+        authorLabel.font = UIFont.systemFont(ofSize: 24)
+        authorLabel.sizeToFit()
+        authorLabel.center = CGPoint(x: view.frame.midX, y: view.frame.maxY - 50)
+        
+        view.addSubview(authorLabel)
+        
     }
 
     override func didReceiveMemoryWarning() {

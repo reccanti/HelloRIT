ab1d829c74377195b67c24538495283e705bcba9
Ben Wilcox <reccanti@gmail.com>
2017-01-26 22:42:56 -0500

    Implement Part 2 changes
    Implements the instructions specified in 'Hello-RIT 2'

--- Hello RIT/ViewController.swift
+++ Hello RIT/ViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     // these are ivars - instance variables
-    var topLabel: UILabel = UILabel()
-    var bottomLabel:UILabel = UILabel()
-    var authorLabel:UILabel = UILabel()
+    var topLabel: UILabel!
+    var bottomLabel:UILabel!
+    var authorLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize top label
+        topLabel = UILabel()
         
         // top label code
         topLabel.text = "Hello"
@@ -28,6 +31,7 @@ class ViewController: UIViewController {
         view.addSubview(topLabel)
         
         // bottom label code
+        bottomLabel = UILabel()
         bottomLabel.text = "RIT"
         bottomLabel.font = UIFont.italicSystemFont(ofSize: 48)
         bottomLabel.sizeToFit()
@@ -40,6 +44,7 @@ class ViewController: UIViewController {
         // view.backgroundColor = UIColor(colorLiteralRed: 0.2, green: 0.5, blue: 0.8, alpha: 1.0)
         
         // add name to the center of the screen programmatically
+        authorLabel = UILabel()
         authorLabel.text = "Ben Wilcox"
         authorLabel.textColor = UIColor.darkGray
         authorLabel.font = UIFont.systemFont(ofSize: 24)
@@ -48,6 +53,40 @@ class ViewController: UIViewController {
         
         view.addSubview(authorLabel)
         
+        // animate top label
+        UIView.animate(withDuration: 0.9,
+                              delay: 0.5,
+             usingSpringWithDamping: 0.9,
+              initialSpringVelocity: 0.0,
+                            options: [],
+                         animations: {
+                                       self.topLabel.center = CGPoint(x:100, y:40+200)
+                                     },
+                         completion: {
+                                       (done:Bool) in
+                                       NSLog("Animation finished - completed = \(done)")
+                                     })
+        
+        // animate bottom label
+        func animateBottomLabel() {
+            self.bottomLabel.center = CGPoint(x:200, y:90+200)
+            self.bottomLabel.alpha = 1.0
+        }
+        UIView.animate(withDuration: 2.0,
+                              delay: 0.5,
+             usingSpringWithDamping: 0.2,
+              initialSpringVelocity: 0.0,
+                            options: [],
+                         animations: animateBottomLabel,
+                         completion: {
+                                        (done:Bool) in
+                                        NSLog("Animation finished - completed = \(done)")
+                                     })
+        
+        // calls viewTapped() whenever this view is tapped
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        view.addGestureRecognizer(tapGesture)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,6 +94,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func viewTapped(tapGesture:UITapGestureRecognizer) {
+        self.animateLabels()
+        print(tapGesture)
+        print(tapGesture.location(in: self.view))
+        print("Num touches=\(tapGesture.numberOfTouches)")
+    }
+    
+    func animateLabels() {
+        self.topLabel.center = CGPoint(x: 100, y: 40)
+        self.bottomLabel.center = CGPoint(x: 200, y:90)
+        self.bottomLabel.alpha = 0.0
+
+        // animate top label
+        UIView.animate(withDuration: 0.9,
+                       delay: 0.5,
+                       usingSpringWithDamping: 0.9,
+                       initialSpringVelocity: 0.0,
+                       options: [],
+                       animations: {
+                        self.topLabel.center = CGPoint(x:100, y:40+200)
+            },
+                       completion: {
+                        (done:Bool) in
+                        NSLog("Animation finished - completed = \(done)")
+        })
+        
+        // animate bottom label
+        func animateBottomLabel() {
+            self.bottomLabel.center = CGPoint(x:200, y:90+200)
+            self.bottomLabel.alpha = 1.0
+        }
+        UIView.animate(withDuration: 2.0,
+                       delay: 0.5,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 0.0,
+                       options: [],
+                       animations: animateBottomLabel,
+                       completion: {
+                        (done:Bool) in
+                        NSLog("Animation finished - completed = \(done)")
+        })
+    }
 
 }
 
